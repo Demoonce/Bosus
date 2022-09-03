f7d2e5884b88c943f4572feab97feb513856da06
Demoonce <pavel.jujkov@gmail.com>
2022-09-03 18:23:32 +0400

    Убрал автоматическое написание анекдотов, теперь оно только по требованию

--- jokes/run.go
+++ jokes/run.go
@@ -2,7 +2,7 @@ package jokes
 
 import (
 	"os"
-	"time"
+	// "time"
 
 	tg "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
@@ -21,19 +21,19 @@ func RunJokes(message *tg.Message) {
 		SendJoke(message.Chat.ID)
 		return
 	}
-	if !JokesStarted {
-		go func() {
-			SendJoke(message.Chat.ID)
-			jokes_ticker := time.NewTicker(time.Minute * 1).C
-			for {
-				select {
-				case <-jokes_ticker:
-					SendJoke(message.Chat.ID)
-				default:
-					time.Sleep(time.Minute * 2)
-				}
-			}
-		}()
-		JokesStarted = true
-	}
+	// if !JokesStarted {
+	// 	go func() {
+	// 		SendJoke(message.Chat.ID)
+	// 		jokes_ticker := time.NewTicker(time.Minute * 1).C
+	// 		for {
+	// 			select {
+	// 			case <-jokes_ticker:
+	// 				SendJoke(message.Chat.ID)
+	// 			default:
+	// 				time.Sleep(time.Minute * 2)
+	// 			}
+	// 		}
+	// 	}()
+	// 	JokesStarted = true
+	// }
 }
